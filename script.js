2791b90f8bb83b9d9c105c1fe22bb1eafff0774a
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-16 20:38:19 -0600

    cash-register 0.0.2
    - button type changed to submit
    - basic styling for web browser display
    - wrote for loop to count total cash in drawer
    - added event listener for submit button
    - initiated processTransaction function
    - function returns change owed as integer
    - wrote basic if statements to check edge cases

--- script.js
+++ script.js
@@ -11,7 +11,35 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
+let totalCashInDrawer = 0;
+drawerSum: for (let i=0; i<cid.length; i++) {
+  totalCashInDrawer += cid[i][1];
+}
+
+console.log('Cash in drawer:', Number(totalCashInDrawer.toFixed(2)))
+
 const changeDue = document.getElementById('change-due');
 const cashInput = document.getElementById('cash');
 const purchaseBtn = document.getElementById('purchase-btn');
 
+purchaseBtn.addEventListener('click', () => {
+  processTransaction(cashInput.value);
+
+})
+
+function processTransaction(cash) {
+  let changeOwed = Math.abs(price - cash);
+  console.log(changeOwed);
+
+  if (changeOwed > totalCashInDrawer) {
+    changeDue.innerText = "Status: INSUFFICIENT_FUNDS";
+  }
+
+  if (changeOwed == 0) {
+    changeDue.innerText = "No change due - customer paid with exact cash";
+  }
+
+  if (changeOwed == totalCashInDrawer) {
+    changeDue.innerText = "Status: CLOSED";
+  }
+}
