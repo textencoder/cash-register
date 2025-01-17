f51bc201e722ae4cdb5937deb1ada2bd9f544d07
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-16 18:15:15 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+
+const changeDue = document.getElementById('change-due');
+const cashInput = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+
