1fa4dd98b6f6eb5014db688e1afe995703583299
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-05 20:16:54 -0600

    established algorithm for retrieving exact change due

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let price = 1.87;
+let price = 2;
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -25,28 +25,21 @@ const purchaseBtn = document.getElementById('purchase-btn');
 //submit click and enter buttons
 purchaseBtn.addEventListener('click', () => {
   processTransaction(cashInput.value);
+  cashInput.value = '';
 })
 
 cashInput.addEventListener('keydown', e => {
   if (e.key == "Enter" || e.key == 13) {
     processTransaction(cashInput.value);
+    cashInput.value = '';
   }
 })
 
 function processTransaction(cash) {
   let changeDue = Number(Math.abs(price - cash).toFixed(2));
-  console.log('original change due', changeDue);
+  console.log('change due', changeDue);
 
-//subtract each denomination from changeDue
-  for (let i=cid.length - 1; i > 0; i--) {
-    if (cid[i][1] > 0.00 && cid[i][1] < changeDue) {
-        changeDue -= cid[i][1];
-        console.log('money taken from', cid[i][1]);
-        console.log('remaining change due', Number(changeDue.toFixed(2)));
-    }
-  }
-
-
+  //initial checks
   if (changeDue > totalCashInDrawer) {
     changeDueDisplay.innerText = "Status: INSUFFICIENT_FUNDS";
   }
@@ -58,4 +51,87 @@ function processTransaction(cash) {
   if (changeDue == totalCashInDrawer) {
     changeDueDisplay.innerText = "Status: CLOSED";
   }
+
+//subtract each denomination from changeDue
+  for (let i=cid.length - 1; i >= 0; i--) {
+    if (cid[i][1] > 0.00) {
+      if (cid[i][0] == 'ONE HUNDRED') {
+        while (changeDue >= 100 && cid[i][1] >= 100) {
+          cid[i][1] -= 100;
+          changeDue -= 100;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$100 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'TWENTY') {
+        while (changeDue >= 20 && cid[i][1] >= 20) {
+          cid[i][1] -= 20;
+          changeDue -= 20;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$20 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'TEN') {
+        while (changeDue >= 10 && cid[i][1] >= 10) {
+          cid[i][1] -= 10;
+          changeDue -= 10;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$10 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'FIVE') {
+        while (changeDue >= 5 && cid[i][1] >= 5) {
+          cid[i][1] -= 5;
+          changeDue -= 5;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$5 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'ONE') {
+        while (changeDue >= 1 && cid[i][1] >= 1) {
+          cid[i][1] -= 1;
+          changeDue -= 1;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$1 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'QUARTER') {
+        while (changeDue >= 0.25 && cid[i][1] >= 0.25) {
+          cid[i][1] -= 0.25; 
+          changeDue -= 0.25;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$0.25 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'DIME') {
+        while (changeDue >= 0.1 && cid[i][1] >= 0.1) {
+          cid[i][1] -= 0.1;
+          changeDue -= 0.1;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$0.1 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'NICKEL') {
+        while (changeDue >= 0.05 && cid[i][1] >= 0.05) {
+          cid[i][1] -= 0.05;
+          changeDue -= 0.05;
+          changeDue = Number(changeDue.toFixed(2))
+          console.log('$0.05 taken from register');
+          console.log(changeDue);
+        }
+      } else if (cid[i][0] == 'PENNY') {
+        while (changeDue >= 0.01 && cid[i][1] >= 0.01) {
+          cid[i][1] -= 0.01;
+          changeDue -= 0.01;
+          changeDue = Number(changeDue.toFixed(2));
+          console.log('$0.01 taken from register');
+          console.log(changeDue);
+        }
+      }
+    }
+  }
+
+
+  
+    
 }
