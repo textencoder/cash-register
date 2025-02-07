c39b303d67ba5350f3ca41ca54fa2ea5817cd685
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-06 19:24:18 -0600

    removed consolelog statements

--- script.js
+++ script.js
@@ -31,9 +31,7 @@ cashInput.addEventListener("keydown", (e) => {
 function processTransaction(cash) {
   let cashParsed = parseFloat(cash);
   cashParsed = Number(cashParsed.toFixed(2));
-  console.log("initial cash input:", cashParsed);
   let changeDue = Number(Math.abs(price - cashParsed).toFixed(2));
-  console.log("change due", changeDue);
 
   //total cash in drawer
   let totalCashInDrawer = 0;
@@ -41,7 +39,6 @@ function processTransaction(cash) {
     totalCashInDrawer += cid[i][1];
   }
   totalCashInDrawer = Number(totalCashInDrawer.toFixed(2));
-  console.log("Cash in drawer:", totalCashInDrawer);
 
   let changeSummary = "";
 
@@ -51,12 +48,10 @@ function processTransaction(cash) {
     return;
   } else if (changeDue > totalCashInDrawer) {
     changeDueDisplay.innerText = "Status: INSUFFICIENT_FUNDS";
-    console.log("else if 1", "Status: INSUFFICIENT_FUNDS");
     return;
   } else if (changeDue == 0) {
     changeDueDisplay.innerText =
       "No change due - customer paid with exact cash";
-    console.log("No change due - customer paid with exact cash");
     return;
   } else {
     //subtract each denomination from changeDue
@@ -69,10 +64,7 @@ function processTransaction(cash) {
             count = Number(count.toFixed(2));
             cid[i][1] -= 100;
             changeDue -= 100;
-
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$100 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += `${cid[i][0]}: $${count}`;
@@ -84,10 +76,7 @@ function processTransaction(cash) {
             count = Number(count.toFixed(2));
             cid[i][1] -= 20;
             changeDue -= 20;
-
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$20 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -100,8 +89,6 @@ function processTransaction(cash) {
             cid[i][1] -= 10;
             changeDue -= 10;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$10 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -114,8 +101,6 @@ function processTransaction(cash) {
             cid[i][1] -= 5;
             changeDue -= 5;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$5 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -128,8 +113,6 @@ function processTransaction(cash) {
             cid[i][1] -= 1;
             changeDue -= 1;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$1 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -142,8 +125,6 @@ function processTransaction(cash) {
             cid[i][1] -= 0.25;
             changeDue -= 0.25;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$0.25 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -157,8 +138,6 @@ function processTransaction(cash) {
             cid[i][1] = Number(cid[i][1].toFixed(2));
             changeDue -= 0.1;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$0.1 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -172,15 +151,12 @@ function processTransaction(cash) {
             cid[i][1] = Number(cid[i][1].toFixed(2));
             changeDue -= 0.05;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$0.05 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
           }
         } else if (cid[i][0] == "PENNY") {
           let count = 0;
-          console.log("penny block cd", changeDue);
           while (changeDue >= 0.01 && cid[i][1] >= 0.01 && changeDue >= 0.001) {
             count += 0.01;
             count = Number(count.toFixed(2));
@@ -188,8 +164,6 @@ function processTransaction(cash) {
             cid[i][1] = Number(cid[i][1].toFixed(2));
             changeDue -= 0.01;
             changeDue = Number(changeDue.toFixed(2));
-            console.log("$0.01 taken from register");
-            console.log(changeDue);
           }
           if (count > 0) {
             changeSummary += ` ${cid[i][0]}: $${count}`;
@@ -204,23 +178,18 @@ function processTransaction(cash) {
     totalCashInDrawer += cid[i][1];
   }
   totalCashInDrawer = Number(totalCashInDrawer.toFixed(2));
-  console.log("final cash in drawer: ", totalCashInDrawer);
   for (let i = cid.length - 1; i >= 0; i--) {
     if (Number(cid[i][1]) > 0) {
-      console.log(cid[i][0] + ":" + cid[i][1]);
     }
   }
 
   if (totalCashInDrawer == 0) {
     changeDueDisplay.innerText = "Status: CLOSED " + changeSummary;
-    console.log("total cash = 0");
   } else if (totalCashInDrawer > 0) {
     changeDueDisplay.innerText = "Status: OPEN " + changeSummary;
-    console.log("Status: OPEN");
   }
 
   if (changeDue) {
     changeDueDisplay.innerText = "Status: INSUFFICIENT_FUNDS";
-    console.log("Status: INSUFFICIENT_FUNDS");
   }
 }
